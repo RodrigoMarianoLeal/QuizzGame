16ece3b2e1508c0b1b9aac8c2dc15e4e33467062
Rodrigo Leal <rodrigoyukata@gmail.com>
2021-06-05 23:57:09 -0300

    Created jogability functions and css

--- frontend/src/components/QuizzCard/index.jsx
+++ frontend/src/components/QuizzCard/index.jsx
@@ -1,31 +1,62 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import {
+  Container, Row, Button, Col,
+} from 'react-bootstrap';
+import './quizzcard.css';
 
-export default function QuizzCard({questions}) {
-
+export default function QuizzCard({ questions }) {
   const [questionNumber, setQuestionNumber] = useState(0);
-  const [questions, setQuestions] = useState(questions);
+  const [questionsData, setQuestions] = useState(questions);
+  const [score, setScore] = useState(0);
+
+  const checkIfCorrect = (value) => {
+    if (questionsData[questionNumber].alternativas[value] === questionsData[questionNumber].correta) {
+      setScore(score + 10);
+    }
+    if ((questionNumber) < 9) {
+      setQuestionNumber(questionNumber + 1);
+    }
+  };
+
   return (
-    <Container className="container">
+    <div className="back">
+      <Col>
+        <Row className="topRow">
+          <Col>
+            <p className="titulo">Quizz Game</p>
+          </Col>
+          <Col>
+            <p className="titulo">
+              {`Score: ${score}`}
+            </p>
+          </Col>
+          <Col>
+            <p className="titulo right">
+              Question Number:
+              {` ${questionNumber + 1}`}
+              /
+              {questionsData.length}
+            </p>
+          </Col>
+        </Row>
 
-      <div className="Titulo"> Quizz Game</div>
-
-      <Container>
-        <Row>
-          <p className="enunciado">{questions[questionNumber].question}</p>
+        <Row className="titulo">
+          <p>
+            Question:
+            {' '}
+            {questionsData[questionNumber].enunciado}
+          </p>
         </Row>
         <Row>
-          <Button></Button>
-          <Button></Button>
+          <Button onClick={() => checkIfCorrect(0)}>{questionsData[questionNumber].alternativas[0]}</Button>
+          <Button onClick={() => checkIfCorrect(1)}>{questionsData[questionNumber].alternativas[1]}</Button>
         </Row>
-        <Row>
-          <Button></Button>
-          <Button></Button>
+        <Row className="bottonRow">
+          <Button onClick={() => checkIfCorrect(2)}>{questionsData[questionNumber].alternativas[2]}</Button>
+          <Button onClick={() => checkIfCorrect(3)}>{questionsData[questionNumber].alternativas[3]}</Button>
         </Row>
-        <Row>
-          <Button>Next</Button>
-        </Row>
-      </Container>
-    </Container>
+      </Col>
+    </div>
   );
 }
